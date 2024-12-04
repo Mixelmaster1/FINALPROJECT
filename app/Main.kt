f6dc5c6c3368286a2c8b25b2dd5399664e8e4445
Xavier Jones <jonesxav002@warren.k12.in.us>
2024-12-04 11:54:58 -0500

    Initial commit

---
+++ app/Main.kt
@@ -0,0 +1,27 @@
+abstract class Main {
+    val weather app = TODO();
+
+    abstract val weather {
+    "Sunny" 5:00 am, 5:10 am
+        "Cloudy" 6:01 am, 6:30am
+        "Rain" 7:51 am, 8:00am
+        "Strom" 8:20 am, 9:55am
+}
+    abstract val weather tomrrow {
+        "Cloudy" 4:00 am, 5:00am
+        "Rain" 6:11 am, 6:30am
+        "Snowfall" 8:00 am, 10:55am
+    }
+
+    abstract val weather next week {
+        "Cloudly with a posiable smow" 5:00 am, 7:12am
+        "Cloudly" 7:32 am, 7:50 am
+        "Windy" 8:10 am, 8:23 am
+        "Snowstorm" 10:20 am, 11:10 am
+
+    }
+
+    abstract val weather "warning a tornado has been repoted" {
+        ""
+    }
+
